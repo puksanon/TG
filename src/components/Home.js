d6726203a611b80ed56da778fdc9f99ed5904431
puksanon <kittipit22@hotmail.com>
2020-02-29 03:00:52 +0700

    update

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,17 @@ import React from 'react'
 import Card  from '../components/home/Card'
 
 class Index extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+          input: ""
+      }
+    }
+
+  myChangeHandler = (event) => {
+    this.setState({input: event.target.value});
+  }
+
 	render() {
 		return (
             <React.Fragment>
@@ -9,9 +20,19 @@ class Index extends React.Component {
                     <div className="container">
                       <div className="text_title mb-4">
                         <h1>ALL LIST</h1>
+                          <h1>{ this.state.input }</h1>
                       </div>
-                      <div className="row">
-                        <Card/>
+                      <div className="search_input mb-4">
+                        <form>
+                              <p>Enter your key:</p>
+                          <input
+                              type='text'
+                              onChange={this.myChangeHandler}
+                          />
+                        </form>
+                      </div>
+                      <div className="card_main">    
+                          <Card input={this.state.input}/>
                       </div>
                     </div>
                 </div>
--- src/components/home/Card.js
+++ src/components/home/Card.js
@@ -2,14 +2,14 @@ import React from 'react';
 import axios from 'axios';
 import './style_card.css'
 import { Card , Button } from 'react-bootstrap';
-  
-class DocumentList extends React.Component {
+
+
+class CardList extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            query: "",
             data: [],
-            filteredData: []
+            search_data: [ ]
         }
     }
 
@@ -24,53 +24,76 @@ class DocumentList extends React.Component {
             })
     }
 
-    handleInputChange = event => {
-        const query = event.target.value;
-    
-        this.setState(prevState => {
-          const filteredData = prevState.data.filter(element => {
-            return element.name.toLowerCase().includes(query.toLowerCase());
-          });
-    
-          return {
-            query,
-            filteredData
-          };
-        });
-      };
-
-
     render() {
-        const { data } = this.state;
-        return (
-            data.map((card , i) => {
-                return (
-                    <div className="col-sm-6 mb-4" key={i}> 
-                        <Card>
-                            <Card.Body>
-                                <blockquote className="blockquote mb-0">
-                                <p className="text-left"> {card.webTitle}</p>
-                                <footer className="blockquote-footer text-left">
-                                    <cite title="Source Title">{ card.sectionName }</cite>
-                                </footer>
-                                </blockquote>
-                            </Card.Body>
-                            <Card.Footer className="text-muted">
-                                <div className="row">
-                                    <div className="col-sm-6">
-                                        <p>{ card.webPublicationDate }</p>
-                                    </div> 
-                                    <div className="col-sm-6">
-                                        <Button href={card.webUrl}>Go to website</Button>
+            return (
+                <div className="row">
+                    {this.state.search_data.length > 0 ? 
+                        (
+                            this.state.search_data.map((card , i) => {
+                                return (
+                                    <div className="col-sm-6 mb-4" key={i}> 
+                                        <Card className="card_doc">
+                                            <Card.Body>
+                                                <blockquote className="blockquote mb-0">
+                                                <p className="text-left"> {card.webTitle}</p>
+                                                <footer className="blockquote-footer text-left">
+                                                    <cite title="Source Title">{ card.sectionName }</cite>
+                                                </footer>
+                                                </blockquote>
+                                            </Card.Body>
+                                            <Card.Footer className="text-muted">
+                                                <div className="row">
+                                                    <div className="col-sm-6">
+                                                        <p>{ card.webPublicationDate }</p>
+                                                    </div> 
+                                                    <div className="col-sm-6">
+                                                        <div className="row">
+                                                            <Button variant="danger" href={card.webUrl} className="mr-1 mb-1">Go to website</Button>
+                                                            <Button variant="info"  className="mr-1 mb-1 ">Read more</Button>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </Card.Footer>
+                                        </Card>
                                     </div>
-                                </div>
-                            </Card.Footer>
-                        </Card>
-                    </div>
-                );
-            })
-        )
+                                );
+                            })
+                        ):
+                        (
+                            this.state.data.map((card , i) => {
+                                return (
+                                    <div className="col-sm-6 mb-4" key={i}> 
+                                        <Card className="card_doc">
+                                            <Card.Body>
+                                                <blockquote className="blockquote mb-0">
+                                                <p className="text-left"> {card.webTitle}</p>
+                                                <footer className="blockquote-footer text-left">
+                                                    <cite title="Source Title">{ card.sectionName }</cite>
+                                                </footer>
+                                                </blockquote>
+                                            </Card.Body>
+                                            <Card.Footer className="text-muted">
+                                                <div className="row">
+                                                    <div className="col-sm-6">
+                                                        <p>{ card.webPublicationDate }</p>
+                                                    </div> 
+                                                    <div className="col-sm-6">
+                                                        <div className="row">
+                                                            <Button variant="danger" href={card.webUrl} className="mr-1 mb-1">Go to website</Button>
+                                                            <Button variant="info"  className="mr-1 mb-1 ">Read more</Button>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </Card.Footer>
+                                        </Card>
+                                    </div>
+                                );
+                            })
+                        )
+                    }
+                </div>    
+            )
     }
 }
 
-export default DocumentList;
+export default CardList ;
